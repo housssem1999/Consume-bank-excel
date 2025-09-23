4b737ab1446f5d48ef224623a55bf18cf8451ed6
Houssem Moussa <62448561+housssem1999@users.noreply.github.com>
2025-09-23 14:39:54 +0000

    Correction de l'importation et simplification de la méthode loadUserByUsername dans UserDetailsServiceImpl

--- src/main/java/com/finance/dashboard/service/UserDetailsServiceImpl.java
+++ src/main/java/com/finance/dashboard/service/UserDetailsServiceImpl.java
@@ -17,7 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsernameOrEmail(username, username)
+        User user = userRepository.findByUsernameOrEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username or email: " + username));
 
         return user;
